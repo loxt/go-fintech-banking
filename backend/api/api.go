5bd1d071e34c79d77c0ca54b1eca587e200a12f0
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-13 02:29:48 -0400

    :sparkles: feat(): register route

--- backend/api/api.go
+++ backend/api/api.go
@@ -16,8 +16,24 @@ type Login struct {
 	Password string
 }
 
+type Register struct {
+	Username string
+	Email    string
+	Password string
+}
+
 type ErrResponse struct {
 	Message string
+}
+
+func StartApi() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/register", register).Methods("POST")
+
+	fmt.Println("App is working on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -38,9 +54,20 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func StartApi() {
-	router := mux.NewRouter()
-	router.HandleFunc("/login", login).Methods("POST")
-	fmt.Println("App is working on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+func register(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+
+	var formattedBody Register
+	err = json.Unmarshal(body, &formattedBody)
+	helpers.HandleErr(err)
+	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
+
+	if register["message"] == "All is fine" {
+		res := register
+		json.NewEncoder(w).Encode(res)
+	} else {
+		res := ErrResponse{Message: "Wrong username or password"}
+		json.NewEncoder(w).Encode(res)
+	}
 }
--- backend/users/users.go
+++ backend/users/users.go
@@ -8,34 +8,7 @@ import (
 	"time"
 )
 
-func Login(username string, pass string) map[string]interface{} {
-	db := helpers.ConnectDB()
-	user := &interfaces.User{}
-
-	if db.Where("username = ?", username).First(&user).RecordNotFound() {
-		return map[string]interface{}{"message": "User not found"}
-	}
-
-	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
-
-	if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
-		return map[string]interface{}{"message": "Wrong password"}
-	}
-
-	var accounts []interfaces.ResponseAccount
-	db.Table("accounts").
-		Select("id, name, balance").
-		Where("user_id = ?", user.ID).Scan(&accounts)
-
-	responseUser := &interfaces.ResponseUser{
-		ID:       user.ID,
-		Username: user.Username,
-		Email:    user.Email,
-		Accounts: accounts,
-	}
-
-	defer db.Close()
-
+func prepareToken(user *interfaces.User) string {
 	tokenContent := jwt.MapClaims{
 		"user_id": user.ID,
 		"expiry":  time.Now().Add(time.Minute ^ 60).Unix(),
@@ -45,10 +18,116 @@ func Login(username string, pass string) map[string]interface{} {
 
 	token, err := jwtToken.SignedString([]byte("TokenPassword"))
 	helpers.HandleErr(err)
+	return token
+}
 
+func prepareResponse(user *interfaces.User, accounts []interfaces.ResponseAccount) map[string]interface{} {
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	var token = prepareToken(user)
 	var response = map[string]interface{}{"message": "All is fine"}
 	response["jwt"] = token
 	response["data"] = responseUser
 
 	return response
+}
+
+func Login(username string, pass string) map[string]interface{} {
+	valid := helpers.Validation([]interfaces.Validation{
+		{
+			Value: username,
+			Valid: "username",
+		},
+		{
+			Value: pass,
+			Valid: "password",
+		},
+	})
+
+	if valid {
+		db := helpers.ConnectDB()
+		user := &interfaces.User{}
+
+		if db.Where("username = ?", username).First(&user).RecordNotFound() {
+			return map[string]interface{}{"message": "User not found"}
+		}
+
+		passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
+
+		if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+			return map[string]interface{}{"message": "Wrong password"}
+		}
+
+		var accounts []interfaces.ResponseAccount
+		db.Table("accounts").
+			Select("id, name, balance").
+			Where("user_id = ?", user.ID).Scan(&accounts)
+
+		defer db.Close()
+
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "Not valid values"}
+	}
+}
+
+func Register(username string, email string, pass string) map[string]interface{} {
+	valid := helpers.Validation([]interfaces.Validation{
+		{
+			Value: username,
+			Valid: "username",
+		},
+		{
+			Value: email,
+			Valid: "email",
+		},
+		{
+			Value: pass,
+			Valid: "password",
+		},
+	})
+
+	if valid {
+		db := helpers.ConnectDB()
+
+		generatePassword := helpers.HashAndSalt([]byte(pass))
+		user := &interfaces.User{
+			Username: username,
+			Email:    email,
+			Password: generatePassword,
+		}
+		db.Create(&user)
+
+		account := &interfaces.Account{
+			Type:    "Daily Account",
+			Name:    string(username + "'s account"),
+			Balance: 0,
+			UserID:  user.ID,
+		}
+
+		db.Create(&account)
+
+		defer db.Close()
+
+		var accounts []interfaces.ResponseAccount
+		respAccount := interfaces.ResponseAccount{
+			ID:      account.ID,
+			Name:    account.Name,
+			Balance: int(account.Balance),
+		}
+
+		accounts = append(accounts, respAccount)
+
+		var response = prepareResponse(user, accounts)
+		return response
+	} else {
+		return map[string]interface{}{"message": "Not valid values"}
+	}
 }
